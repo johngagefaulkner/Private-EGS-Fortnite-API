676b8b113910acb1457cd1e508572c1422fa8483
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-03-12 18:47:31 -0400

    Update Get-STWMissions.ps1

--- Data/Get-STWMissions.ps1
+++ Data/Get-STWMissions.ps1
@@ -1,17 +1,29 @@
-# Get STW Missions without auth
+<#
+  - Name: Get-STWMissions.ps1
+  - Description: This PowerShell script obtains the raw JSON data of all available Daily Missions in Save the World WITHOUT requiring an authorization Access Token.
+#>
+
+# Configure script action preferences
 $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = 'Stop'
 
+# Clear the PowerShell console, then display script info
 Clear-Host
-Write-Host "[ Get-STWMissions.ps1 ]"
-Write-Host "Retrieving Access Token from NiteStats... " -NoNewline
+Write-Host "[ Get-STWMissions.ps1 ]" -ForegroundColor Magenta
 
+# Step 1: Retrieve an Access Token (not tied to a user) from the NiteStats API.
+Write-Host " 1. Retrieving generic Access Token from NiteStats API... " -NoNewline
 $niteToken = Invoke-RestMethod -Method GET -Uri "https://api.nitestats.com/v1/epic/bearer" | Select-Object -Expand "accessToken"
-
 Write-Host "Done!" -ForegroundColor Green
+
+# Step 2: Use the Access Token from the previous step to authenticate our request for the daily missions data.
 Write-Host "Retrieving current daily Save the World missions... " -NoNewline
-
-$stwMissions = curl "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/world/info?lang=en-US" -H "Authorization: Bearer $niteToken" --silent --output "C:\Users\Public\FortniteAPI-Game-v2-worldInfo-en.json"
-
+## Define where you want to save/export the JSON response data
+$outputFilePath = "C:\Users\Public\FortniteAPI-Game-v2-worldInfo-en.json"
+## Send the API Request
+$stwMissions = curl "https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/world/info?lang=en-US" -H "Authorization: Bearer $niteToken" --silent --output "$outputFilePath"
 Write-Host "Done!" -ForegroundColor Green
-$userInput = Read-Host -Prompt "Operation completed successfully. Press Enter to exit."
+
+# Gracefully exit
+Write-Host "File successfully exported to: $outputFilePath"
+Read-Host -Prompt "Operation completed successfully! Press Enter to exit."
